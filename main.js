a436db97c16d22c6eca0686be8fde86fd80ac9e9
jaiswalk008 <jaiskaran008@gmail.com>
2023-06-25 20:28:59 +0530

    completed the project with every function in async/await form

--- main.js
+++ main.js
@@ -1,76 +1,58 @@
 const form = document.getElementById('my-form');
 form.addEventListener('submit', addExpense);
 const list =document.querySelector('.expense-list');
-list.addEventListener('click',change);
 
-//function to add list elemenst
-function addli(info){
-    let newLi = document.createElement('li');
-    //delete button
-    let deleteBtn = document.createElement('button');
 
-    deleteBtn.textContent='Delete Expense';
-    newLi.style.margin='5px';
-    deleteBtn.style.marginLeft='5px';
-    deleteBtn.className='delete';
-    //edit button
-    let editBtn = document.createElement('button');
-    editBtn.textContent='Edit Expense';
-    editBtn.style.marginLeft='5px';
-    editBtn.className='edit';
-    newLi.appendChild(document.createTextNode(info));
-    newLi.appendChild(editBtn);
-    newLi.appendChild(deleteBtn);
-    return newLi;
+// function to add list elemenst
+function addExpenseInfo(info){
+    let expense = document.createElement('li');
+    //adding onClick handlers on edit button and delete an slo passing the id of expense as a parameter
+    expense.innerHTML=`<span>${info.amount} - ${info.description} - ${info.category}</span>
+    <button onClick="editExpense(${info.id})" class="edit btn-sm btn-secondary">edit</button>
+    <button onClick="deleteExpense(${info.id})" class="delete btn-sm btn-dark">delete</button>`;
+    list.appendChild(expense);
 }
 //function to add expense
-function addExpense(e){
+async function addExpense(e){
     e.preventDefault();
-    let expenseDetails = {
+    let expense = {
         amount: e.target.expense.value,
         description: e.target.description.value,
         category :e.target.category.value
     };
+    
+    try{
+        const expenseDetails = await axios.post('http://localhost:3000/',expense);
+        addExpenseInfo(expenseDetails.data);
+    }catch(err){console.log(err)}
 
-    let info = expenseDetails.amount +' - '+expenseDetails.description+' - '+expenseDetails.category;
-    console.log(info);
-    list.appendChild(addli(info));
-
-    localStorage.setItem(''+expenseDetails.description,JSON.stringify(info));
     form.reset();
 }
-if(localStorage.length) show();
+window.addEventListener('DOMContentLoaded',async () =>{
+    try{
+        const expenseDetails = await axios.get('http://localhost:3000/');
+        //using HOF as data is in array 
+        expenseDetails.data.forEach((e) => addExpenseInfo(e))
+    }catch(err){console.log(err)}
+})
 
-//function to show the expense details after page reloads
-function show(){
-    let keys = Object.keys(localStorage);
+async function editExpense(id){
+    try{
+        const expense= await axios.get(`http://localhost:3000/editExpense/${id}`);
+        document.getElementById('expense').value = expense.data.amount;
+        document.getElementById('description').value = expense.data.description;
+        document.getElementById('category').value = expense.data.category;
+        //calling delete function to remove that expense
+        deleteExpense(id);
+    }catch(err){console.log(err)};
+    
 
-    for(let i=0;i<keys.length;i++){
-        let expenseDetails = JSON.parse(localStorage.getItem(keys[i]));
-        list.appendChild(addli(expenseDetails));
-    }
 }
-function change(e){
-    let li = e.target.parentElement;
-    let arr = li.childNodes[0].textContent.split(' - ');
-    console.log(arr);
-    //delete functionality
-    if(e.target.classList.contains('delete')){
-        if(confirm('Are you sure to delete?')){
-            list.removeChild(li);
-            localStorage.removeItem(arr[1]);
-        }
+async function deleteExpense(id){
+    try{
+        await axios.delete(`http://localhost:3000/deleteExpense/${id}`);
+        //th code inside the parathesis will return the li element to be deleted
+        list.removeChild(document.querySelector('.delete').parentElement)
     }
-    //edit functionality
-    if(e.target.classList.contains('edit')){
-        //removing user data to edit
-        list.removeChild(li);
-        //setting field values
-        
-        document.getElementById('expense').value=arr[0];
-        document.getElementById('description').value=arr[1];
-        document.getElementById('category').value=arr[2];
-        localStorage.removeItem(arr[1]);
-
-    }
+    catch(err){console.log(err);}
 }
